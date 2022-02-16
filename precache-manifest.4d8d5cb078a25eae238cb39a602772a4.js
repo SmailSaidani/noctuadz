da10471a3c8fc475a37a28bf4462e870f2557dba
SmailSaidani <smailsai98@gmail.com>
2022-02-16 09:19:40 -0800

    Updates

---
+++ precache-manifest.4d8d5cb078a25eae238cb39a602772a4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f18039d8afe50be3c15",
+    "url": "/smailsai/static/css/main.d5fc66ed.chunk.css"
+  },
+  {
+    "revision": "3f18039d8afe50be3c15",
+    "url": "/smailsai/static/js/main.7e1b1e3a.chunk.js"
+  },
+  {
+    "revision": "cdcdbf7d3940590b7d18",
+    "url": "/smailsai/static/js/runtime~main.dba94ca0.js"
+  },
+  {
+    "revision": "14dd65137e2722bbe84c",
+    "url": "/smailsai/static/css/2.272ed8bb.chunk.css"
+  },
+  {
+    "revision": "14dd65137e2722bbe84c",
+    "url": "/smailsai/static/js/2.bdac4218.chunk.js"
+  },
+  {
+    "revision": "60a4d80fdbb12a1a0425e10a1990094e",
+    "url": "/smailsai/static/media/backdrop.60a4d80f.png"
+  },
+  {
+    "revision": "83b39c088612d915d5378842db4ee0b2",
+    "url": "/smailsai/static/media/Logo.83b39c08.svg"
+  },
+  {
+    "revision": "430decf2805e2c94b711db9849f6114c",
+    "url": "/smailsai/static/media/hangup.430decf2.svg"
+  },
+  {
+    "revision": "cb5a271abb22da2c93866b972f0f62c4",
+    "url": "/smailsai/static/media/more-vertical.cb5a271a.svg"
+  },
+  {
+    "revision": "745449c2d1946743699328ae1701c956",
+    "url": "/smailsai/static/media/copy.745449c2.svg"
+  },
+  {
+    "revision": "31619c4ebbb03f8fd4cf1c1878d09d56",
+    "url": "/smailsai/index.html"
+  }
+];
